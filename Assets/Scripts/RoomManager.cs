48de44a671dac393d39cb5c650e8c36ea59e44cc
antoine.martin <antoine.martin885@gmail.com>
2022-11-15 16:06:31 +0100

    update room manager

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -1,30 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
     public GameObject RoomPrefab;
-
     public int NumberofRoomsActives = 0;
     public Room CurrentRoom => _currentRoom;
     public List<Room> Rooms;
-    [Range(0, 0.01f)]
-    public float RoomSpeed = 0.001f;
+    [Range(0, 0.05f)] public float RoomSpeed = 0.001f;
 
     private Room _currentRoom;
     private int _roomId;
+
+
+    public GameObject CURRENTROOM;
+
     void Start()
     {
         _roomId = 0;
         Rooms = new List<Room>();
-        
-        for(int i = 0; i < NumberofRoomsActives; i ++)
+
+        for (int i = 0; i < NumberofRoomsActives; i++)
         {
             CreateRoom();
         }
+
+        _currentRoom = Rooms[0];
     }
-    
+
     /// <summary>
     /// Creates a room behind the last one in the list
     /// </summary>
@@ -46,12 +49,14 @@ public class RoomManager : MonoBehaviour
 
         Rooms.Add(room);
     }
-    
+
     private void DeleteFirstRoomInList()
     {
         GameObject door = Rooms[0].gameObject;
         Rooms.RemoveAt(0);
         Destroy(door);
+
+        _currentRoom = Rooms[0];
     }
 
     // Update is called once per frame
@@ -62,17 +67,37 @@ public class RoomManager : MonoBehaviour
         {
             MoveOnZ(door.transform);
         }
-        
+
         // Detect if the first element in the list is still see by the player
         if (Rooms[0].transform.position.z <= -10)
         {
             DeleteFirstRoomInList();
-            
+
             // If a door is removed, add a new one after the remaining ones
             CreateRoom();
         }
+
+        // Check current room
+        if (CheckCurrentRoom() == false)
+        {
+            _currentRoom = Rooms[1];
+        }
+
+        CURRENTROOM = _currentRoom.gameObject;
+
+        //  DEBUG : Force Open door
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            OpenCurrentDoor();
+            Debug.Log(_currentRoom.EmotionForOpening);
+        }
     }
-    
+
+    private bool CheckCurrentRoom()
+    {
+        return Rooms[0].transform.position.z > -5;
+    }
+
     private void MoveOnZ(Transform tf)
     {
         tf.position -= tf.forward * RoomSpeed;
@@ -80,6 +105,6 @@ public class RoomManager : MonoBehaviour
 
     public void OpenCurrentDoor()
     {
-        
+        CurrentRoom.OpenDoor();
     }
 }
